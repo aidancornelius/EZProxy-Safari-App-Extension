2d3eebfc19bf616ebc22908ae6694de9fdc92bfe
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2018-09-18 11:58:16 +0930

    Initial Commit

---
+++ EZProxy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EZProxy
+//
+//  Created by Aidan Cornelius-Bell on 18/9/18.
+//  Copyright © 2018 Aidan Cornelius-Bell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
